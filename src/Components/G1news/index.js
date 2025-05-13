94d6a0b8275f7841129b8de8e7c8e27c8362e2e1
Dev Erick <157327151+ErickMenezes321995@users.noreply.github.com>
2025-05-13 12:57:37 -0400

    alterações

--- src/Components/G1news/index.js
+++ src/Components/G1news/index.js
@@ -8,7 +8,9 @@ import {
   Typography,
   CardActionArea,
   Paper,
-  Skeleton
+  Skeleton,
+  CircularProgress,
+  Box,
 } from "@mui/material";
 
 function Noticia() {
@@ -16,7 +18,8 @@ function Noticia() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('https://bairro-manaus.onrender.com/api/noticias')
+    axios
+      .get("https://bairro-manaus.onrender.com/api/noticias")
       .then((res) => {
         setNoticias(res.data);
         setLoading(false);
@@ -27,49 +30,72 @@ function Noticia() {
       });
   }, []);
 
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          minHeight: "100vh", // ocupa a tela toda
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          bgcolor: "#002f6c", // azul escuro
+          color: "#fff",
+          textAlign: "center",
+        }}
+      >
+        <CircularProgress color="inherit" />
+        <Typography variant="h6" mt={2}>
+          Buscando notícias...
+        </Typography>
+      </Box>
+    );
+  }
+
   return (
     <Paper elevation={3} sx={{ p: 3, bgcolor: "#fff" }}>
       <Grid container spacing={3} justifyContent="center">
-        {loading
-          ? Array.from(new Array(3)).map((_, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index}>
-                <Skeleton variant="rectangular" height={200} />
-                <Skeleton />
-                <Skeleton width="60%" />
-              </Grid>
-            ))
-          : noticias.map((noticia, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index}>
-                <Card sx={{ maxWidth: 345, mx: "auto", height: "100%", display: "flex", flexDirection: "column" }}>
-                  <CardActionArea
-                    component="a"
-                    href={noticia.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {noticia.imagem && (
-                      <CardMedia
-                        component="img"
-                        height="180"
-                        image={noticia.imagem}
-                        alt={noticia.titulo}
-                      />
-                    )}
-                    <CardContent>
-                      <Typography gutterBottom variant="h6" component="div">
-                        {noticia.titulo}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {noticia.resumo}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Grid>
-            ))}
+        {noticias.map((noticia, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card
+              sx={{
+                maxWidth: 345,
+                mx: "auto",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <CardActionArea
+                component="a"
+                href={noticia.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {noticia.imagem && (
+                  <CardMedia
+                    component="img"
+                    height="180"
+                    image={noticia.imagem}
+                    alt={noticia.titulo}
+                  />
+                )}
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {noticia.titulo}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {noticia.resumo}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Paper>
   );
 }
 
 export default Noticia;
+
