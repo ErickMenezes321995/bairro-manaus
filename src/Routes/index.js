e576fbc66a2dd6116d8f54b8365ad3b2918fcfbb
Dev Erick <157327151+ErickMenezes321995@users.noreply.github.com>
2025-05-20 21:28:59 -0400

    alteração no serviços

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,4 @@
-
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from '../Pages/Home';
 import Sobre from '../Pages/Sobre';
@@ -9,20 +8,17 @@ import Informações from '../Pages/Noticias';
 import Serviços from '../Pages/Serviços';
 import ProducingRoute from '../Components/Produção/indexRoute';
 
-
-function RoutesApp(){
-    return(
-   
-       <Routes>
-        <Route path='/' element={  <ProducingRoute ativo={false}> <Home/> </ProducingRoute>}/>
-        <Route path='/sobre' element={ <ProducingRoute ativo={false}> <Sobre /> </ProducingRoute> }/>
-        <Route path='/Contato' element={ <ProducingRoute ativo={false}> <Contato/> </ProducingRoute>} />
-        <Route path='/Mercadao' element={ <ProducingRoute ativo={false}> <Mercadao/> </ProducingRoute> } />
-        <Route path='/Informações' element={ <ProducingRoute ativo={false}> <Informações/> </ProducingRoute> } />
-        <Route path='/serviços' element={ <ProducingRoute ativo={true}> <Serviços/> </ProducingRoute> } />
-       </Routes>
-    
-    )
+function RoutesApp() {
+    return (
+        <Routes>
+            <Route path='/' element={<ProducingRoute ativo={false}><Home /></ProducingRoute>} />
+            <Route path='/sobre' element={<ProducingRoute ativo={false}><Sobre /></ProducingRoute>} />
+            <Route path='/contato' element={<ProducingRoute ativo={false}><Contato /></ProducingRoute>} />
+            <Route path='/mercadao' element={<ProducingRoute ativo={false}><Mercadao /></ProducingRoute>} />
+            <Route path='/informações' element={<ProducingRoute ativo={false}><Informações /></ProducingRoute>} />
+            <Route path='/serviços' element={<ProducingRoute ativo={false}><Serviços /></ProducingRoute>} />
+        </Routes>
+    );
 }
 
 export default RoutesApp;
